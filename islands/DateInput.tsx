aa21e69ccc8d9c309a8022a1a50f3bedab655200
Christoph Müller <mueller@olyro.de>
2025-01-12 19:17:16 +0100

    basic component for adding todos

--- islands/DateInput.tsx
+++ islands/DateInput.tsx
@@ -1,15 +1,41 @@
 import * as chrono from "npm:chrono-node@2.7.7";
-import { useSignal } from "@preact/signals";
+import { Signal } from "@preact/signals";
 import { Button } from "../components/Button.tsx";
 
-export default function DateInput() {
-  const d = useSignal<Date | undefined>(undefined);
+export type Todo = {
+  start?: Date;
+  end?: Date;
+  text: string;
+};
 
+type DateProps = {
+  todo: Signal<Todo | null>;
+};
+
+export default function DateInput(props: DateProps) {
   return (
     <div class="flex gap-8 py-6">
-        <input class={"p-2 rounded-md border-gray-200 border outline-none"} onChange={(e) => {d.value = (chrono.de.parseDate((e.target as any).value))}}></input>
-        <Button>Add</Button>
-        {d}
+      <input
+        class={"p-2 rounded-md border-gray-200 border outline-none"}
+        onInput={(e) => {
+          const text = (e.target as any).value as string;
+          const results = chrono.de.parse(text);
+          const result = results.length > 0 ? results[0] : undefined;
+
+          if (result) {
+            console.log("i was here");
+            props.todo.value = {
+              start: result.start.date(),
+              end: result.end?.date(),
+              text: text.replace(result.text, ""),
+            };
+          } else {
+            props.todo.value = null;
+          }
+        }}
+      >
+      </input>
+      <Button>Add</Button>
     </div>
   );
 }
