c3ad7a1a2cd72816eafa91e28bb04585e621bbc9
Christoph Müller <mueller@olyro.de>
2025-01-17 00:00:54 +0100

    started work on dockerfile

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM denoland/deno:alpine-2.1.5
+
+WORKDIR /app
+
+COPY . .
+RUN rm -rf node_modules
+RUN deno cache --reload --allow-import main.ts
+RUN deno task build
+
+USER deno
+EXPOSE 8000
+
+CMD ["run", "-A", "--unstable-cron", "--unstable-kv", "main.ts"]
--- fresh.config.ts
+++ fresh.config.ts
@@ -9,22 +9,27 @@ const oldTodos = new ExpiryMap<string, Todo>(1000 * 240);
 const BOT_TOKEN = Deno.env.get("BOT_TOKEN");
 const CHAT_ID = Deno.env.get("CHAT_ID");
 
-Deno.cron("notification cron", { minute: { every: 1 } }, async () => {
-  const kv = await Deno.openKv();
-  const iter = kv.list<Todo>({ prefix: ["todos"] });
-  const now = new Date();
-  for await (const res of iter) {
-    if (res.value.start) {
-      if (Math.abs(now.getTime() - new Date(res.value.start).getTime()) < 90 * 1000 && !oldTodos.has(res.value.id)) {
-        oldTodos.set(res.value.id, res.value);
-        if (BOT_TOKEN && CHAT_ID) {
-          const bot = new Bot(BOT_TOKEN);
-          bot.api.sendMessage(CHAT_ID, res.value.text);
+const isBuildMode = Deno.args.includes("build");
+
+if (!isBuildMode) {
+  Deno.cron("notification cron", { minute: { every: 1 } }, async () => {
+    const kv = await Deno.openKv();
+    const iter = kv.list<Todo>({ prefix: ["todos"] });
+    const now = new Date();
+    for await (const res of iter) {
+      if (res.value.start) {
+        if (Math.abs(now.getTime() - new Date(res.value.start).getTime()) < 90 * 1000 && !oldTodos.has(res.value.id)) {
+          oldTodos.set(res.value.id, res.value);
+          if (BOT_TOKEN && CHAT_ID) {
+            const bot = new Bot(BOT_TOKEN);
+            bot.api.sendMessage(CHAT_ID, res.value.text);
+          }
         }
       }
     }
-  }
-});
+  });
+}
+
 
 export default defineConfig({
   plugins: [tailwind()],
