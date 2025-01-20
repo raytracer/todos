631b787baaadbada1d81d038ce52fd9220850d5c
Christoph Müller <mueller@olyro.de>
2025-01-20 12:11:42 +0700

    cursor improvements

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,7 +6,7 @@ export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
     <button
       {...props}
       disabled={!IS_BROWSER || props.disabled}
-      class="p-2 border-gray-500 border rounded bg-white hover:bg-gray-200 transition-colors h-full"
+      class="p-2 disabled:cursor-not-allowed disabled:text-gray-400 whitespace-nowrap border-gray-500 border rounded bg-white hover:bg-gray-200 transition-colors h-full"
     />
   );
 }
